9b7662024a8b0e32d549afd94835611f6efa0b47
theerath <theerathgeddada@gmail.com>
2019-11-28 16:49:46 +0530

    v2: Added dtypes, median,mode for numeric columns, columns organized and renamed. added missing values column and respective percentages

--- BasicStatistics/basicstatistics.py
+++ BasicStatistics/basicstatistics.py
@@ -35,10 +35,12 @@ class statistics(object):
     @staticmethod
     def num_stats(df):
         num_stats=df.describe().T
+        num_stats['median']=df.quantile(0.5)
+        num_stats['mode']=df.mode().iloc[0]
         num_stats['kurtosis']=df.kurt()
         num_stats['skewness']=df.skew()
         num_stats['variance']=df.var()
-        num_stats['coeef_of_variation']=num_stats['std']/num_stats['mean'] if num_stats.mean else np.nan
+        num_stats['coef_of_variation']=num_stats['std']/num_stats['mean'] if num_stats.mean else np.nan
         num_stats['mean_absolute_deviation']=df.mad()
         num_stats['iqr']=num_stats['75%']-num_stats['25%']
         num_stats['1%']=df.quantile(0.01)
@@ -56,17 +58,22 @@ class statistics(object):
     @staticmethod
     def freq(df, f_type):
         if f_type == 'min':
-            s = df.apply(lambda x: x.value_counts().min() if (x.nunique() > 1) else None, axis=0).rename('min_freq')
+            s1 = df.apply(lambda x: x.value_counts().min() if (x.nunique() > 1) else None, axis=0).rename('min_freq')
+            s2 = df.apply(lambda x: x.value_counts().reset_index()['index'].iloc[-1] if (x.nunique() > 1) else None, axis=0).rename('min_freq_category')
+            s=pd.concat([s1,s2],axis=1)
         if f_type == 'second':
-            s = df.apply(lambda x: x.value_counts()[1] if (x.nunique() > 1) else None, axis=0).rename('second_freq')
+            s1 = df.apply(lambda x: x.value_counts()[1] if (x.nunique() > 1) else None, axis=0).rename('2nd_freq')
+            s2 = df.apply(lambda x: x.value_counts().reset_index()['index'].iloc[1] if (x.nunique() > 1) else None, axis=0).rename('2nd_freq_category')
+            s = pd.concat([s1, s2], axis=1)
         return s
 
     def cat_stats(self,df):
         cat_stats=df.describe().T
+        cat_stats=cat_stats.rename(columns={'top':'top_freq_category','freq':'top_freq'})
         cat_stats = cat_stats.merge(self.freq(df, 'second'), left_index=True, right_index=True, how='left')
         cat_stats = cat_stats.merge(self.freq(df, 'min'), left_index=True, right_index=True, how='left')
-        cat_stats['freq_ratio_to_second'] = cat_stats['freq'] / cat_stats['second_freq']
-        cat_stats['freq_ratio_to_min'] = cat_stats['freq'] / cat_stats['min_freq']
+        cat_stats['top_freq/2nd_freq'] = cat_stats['top_freq'] / cat_stats['2nd_freq']
+        cat_stats['top_freq/min_freq'] = cat_stats['top_freq'] / cat_stats['min_freq']
         cat_stats = cat_stats.reset_index().rename(columns={'index': 'Feature'})
         return cat_stats
 
@@ -85,6 +92,39 @@ class statistics(object):
         df[name]=df[a]+df[b]
         df.drop([a,b],inplace=True, axis=1)
         return df
+
+    @staticmethod
+    def missing(df):
+
+        """
+        Calling this method will return the count and percentage of missing values in the data
+
+        :return: Returns a dataframe with three columns- feature,missing_count,missing_perc
+        """
+        missing_count = df.apply(lambda x: sum(x.isnull().values), axis=0).rename('missing_count')
+        missing_perc = df.apply(lambda x: sum(x.isnull().values), axis=0).rename('missing_perc') / df.shape[0]
+        missing = pd.concat([missing_count, missing_perc], axis=1)
+        return missing.reset_index().rename(columns={'index':'Feature'})
+
+    @staticmethod
+    def dtypes(df):
+
+        """
+        Calling this method will return the count and percentage of missing values in the data
+
+        :return: Returns a dataframe with three columns- feature,missing_count,missing_perc
+        """
+
+        return pd.DataFrame(df.dtypes).reset_index().rename(columns={'index':'Feature',0:'dtype'})
+
+    @staticmethod
+    def reorganize(df):
+        common_stats=['Feature','dtype','count','unique_count','missing_count','missing_perc']
+        num_stats=['min','25%','50%','75%','max','mean','std','median','mode','kurtosis','skewness','variance','coef_of_variation','mean_absolute_deviation','iqr']
+        outlier_stats=['1%','99%','5%','95%','outliers','outliers%','num_zeros','sum']
+        cat_stats=['top_freq_category','top_freq','2nd_freq_category','2nd_freq','min_freq_category','min_freq','top_freq/2nd_freq','top_freq/min_freq']
+        cols=common_stats+num_stats+outlier_stats+cat_stats
+        return df[cols]
 
     def stat(self):
         """
@@ -107,7 +147,10 @@ class statistics(object):
         stats_df=stats_df.merge(stats_cat,how='left',on='Feature')
         stats_df=self.combine(stats_df,'count_x','count_y','count')
         stats_df = self.combine(stats_df, 'unique_num', 'unique', 'unique_count')
+        stats_df=stats_df.merge(self.missing(self.df),how='left',on='Feature')
+        stats_df = stats_df.merge(self.dtypes(self.df), how='left', on='Feature')
         stats_df=stats_df.fillna('-')
+        stats_df=self.reorganize(stats_df)
         return stats_df
 
 
